b69097d997133e2225c2b6a31346e9c8637a8379
ohdoking <ohdoking@gmail.com>
2017-03-08 01:13:28 +0900

    나머지 옵저버 추가 및 테스트 코드 추

--- src/com/ohdoking/designpattern/day2/WeatherStation.java
+++ src/com/ohdoking/designpattern/day2/WeatherStation.java
@@ -8,7 +8,14 @@ public class WeatherStation {
 		
 		CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
 	
-		weatherData.setMeasurements(80,  65,  30.4f);
+		StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+		ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+		HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+
+		weatherData.setMeasurements(80, 65, 30.4f);
+		weatherData.setMeasurements(82, 70, 29.2f);
+		weatherData.setMeasurements(78, 90, 29.2f);
+		
 	
 	}
 

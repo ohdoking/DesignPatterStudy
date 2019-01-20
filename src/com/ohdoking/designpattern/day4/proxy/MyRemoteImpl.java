8925589685ea48e257a32767d2975e50ed5c9344
Kenny Oh <ohdoking@gmail.com>
2019-01-20 17:29:00 +0100

    add ip

--- src/com/ohdoking/designpattern/day4/proxy/MyRemoteImpl.java
+++ src/com/ohdoking/designpattern/day4/proxy/MyRemoteImpl.java
@@ -2,6 +2,8 @@ package com.ohdoking.designpattern.day4.proxy;
 
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
@@ -19,6 +21,7 @@ public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
 		MyRemote myRemote;
 		try {
 			myRemote = new MyRemoteImpl();
+			System.setProperty("java.rmi.server.hostname","192.168.1.2");
 			Naming.rebind("RemoteHello", myRemote);
 		} catch (Exception e) {
 			e.printStackTrace();

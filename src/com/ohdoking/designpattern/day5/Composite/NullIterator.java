44e3b0a8979281aba863d720ccc54611221b76f7
Kenny Oh <ohdoking@gmail.com>
2019-02-06 11:50:08 +0100

    implement nulliteractor class

---
+++ src/com/ohdoking/designpattern/day5/Composite/NullIterator.java
@@ -0,0 +1,20 @@
+package com.ohdoking.designpattern.day5.Composite;
+
+import java.util.Iterator;
+
+public class NullIterator implements Iterator {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}

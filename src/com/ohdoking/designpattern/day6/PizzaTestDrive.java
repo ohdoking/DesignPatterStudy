5284d1c57de78359285af946095627e597733ca0
Kenny Oh <ohdoking@gmail.com>
2019-02-07 11:30:44 +0100

    to apply to simple factory design pattern.

---
+++ src/com/ohdoking/designpattern/day6/PizzaTestDrive.java
@@ -0,0 +1,19 @@
+package com.ohdoking.designpattern.day6;
+
+import com.ohdoking.designpattern.day6.factory.SimplePizzaFactory;
+
+public class PizzaTestDrive {
+ 
+	public static void main(String[] args) {
+		SimplePizzaFactory factory = new SimplePizzaFactory();
+		PizzaStore store = new PizzaStore(factory);
+
+		Pizza pizza = store.orderPizza("cheese");
+		System.out.println("We ordered a " + pizza.getName() + "\n");
+		System.out.println(pizza);
+ 
+		pizza = store.orderPizza("veggie");
+		System.out.println("We ordered a " + pizza.getName() + "\n");
+		System.out.println(pizza);
+	}
+}
